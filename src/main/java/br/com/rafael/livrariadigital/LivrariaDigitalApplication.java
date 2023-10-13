0afe83dd4d0755d7f9a4d4f7193ab19fad7727db
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-13 15:17:31 -0300

    Criando Projeto

---
+++ src/main/java/br/com/rafael/livrariadigital/LivrariaDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.livrariadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivrariaDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rafael/livrariadigital/LivrariaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.livrariadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
